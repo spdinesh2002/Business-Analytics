318b039d112603f6eb5164554bb0d4fcc7a7bbb7
spdinesh2002 <dinesh.sp.2019.cse@rajalakshmi.edu.in>
2023-03-27 01:10:09 +0530

    updated

--- code/env/app.py
+++ code/env/app.py
@@ -8,7 +8,10 @@ model=pickle.load(open("BA_Model.pkl","rb"))
 def index():
     return render_template('index.html')
  
- 
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
 @app.route('/predict',methods=["POST"])
 def predict():
@@ -23,16 +26,16 @@ def predict():
     result=model.predict(input_array)
     if result[0]=="Revenue will decrease,offer sale promotions to boost the volume of sales":
         output_message="Focus on your customers, increase your marketing and sales efforts, review your pricing strategies, and expand your market to increase revenue for your company."
-        return render_template("index.html",Result="{}".format(output_message))
+        return render_template("form.html",Result="{}".format(output_message))
     elif result[0]=="Revenue will increase":
         output_message="The revenue will rise. Develop new product or service lines, sell more to current clients, and run sales campaigns to increase volume of sales."
-        return render_template("index.html",Result="{}".format(output_message))
+        return render_template("form.html",Result="{}".format(output_message))
     elif result[0]=="Sales price will decrease,concentrate on right taget":
         output_message="Sales prices will fall as the number of customers grows. Increase the size of the average transaction. Boost the number of transactions per customer. Increase your prices."
-        return render_template("index.html",Result="{}".format(output_message))
+        return render_template("form.html",Result="{}".format(output_message))
     else:
         output_message="loss of income. in order to earn more Establish a budget, Pay attention to your profit margins, Review the profitability of your company. benchmark the performance of your business, evaluate the success of cost-management initiatives, Assess business productivity. Create fresh business strategies."
-        return render_template("index.html",Result="{}".format(output_message))
+        return render_template("form.html",Result="{}".format(output_message))
 
     
 
